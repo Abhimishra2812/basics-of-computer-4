3ff4e56fd73b138f61e1a86c4dd2e3f786d9d7a1
Abhimishra2812 <101655063+Abhimishra2812@users.noreply.github.com>
2022-03-16 12:00:25 +0530

    Add files via upload

---
+++ basics of computer 4.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+get_ipython().set_next_input('1 )Write a step wise process on how to create a new user');get_ipython().run_line_magic('pinfo', 'user')
+Ans:To create a new user account:
+    .Choose Start→Control Panel and in the resulting window, click the Add or Remove User Accounts link. ...
+    .Click Create a New Account. ...
+    .Enter an account name and then select the type of account you want to create. ...
+    .Click the Create Account button and then close the Control Panel.
+    
+get_ipython().set_next_input('2 )When do we use Log off and when do we use shut down');get_ipython().run_line_magic('pinfo', 'down')
+Ans:Log Off: Closes all your files and programs and logs you off from Windows without turning off the computer.
+    Shutdown: Turns off the power to your computer.
+        
+get_ipython().set_next_input('3 )Define what is a hard disk and write a step wise process to create a folder on your name on your hard disk');get_ipython().run_line_magic('pinfo', 'disk')
+Ans:a piece of hard plastic that is fixed inside a computer and is used for storing data and programs permanently.
+    To create a folder on your hard disk here are the steps:
+    .With your document open, click File > Save As.
+    .Under Save As, select where you want to create your new folder. ...
+    .In the Save As dialog box that opens, click New Folder.
+    .Type the name of your new folder, and press Enter. ...
+    .Click Save.
+    
+get_ipython().set_next_input('4 )Write step wise process to open the folder that you have created on your name');get_ipython().run_line_magic('pinfo', 'name')
+Ans:To open a folder that you have created on your name.Follow the steps:
+    .Launch the File Open dialog. In the File menu, select the Open menu item.
+    .Open the folder containing the desired file. ...
+    .Select the desired file within the folder. ... (Optional) Specify a file format type. (Optional) Specify the character encoding.
+    .Click on the Open button.
+    
+5 )Write a step wise process to rename the folder on your Mother’s Name.
+Ans:To rename the folder on your Mother’s Name here are some steps:
+    .Right-click on the item and select Rename, or select the file and press F2 .
+    .Type the new name and press Enter or click Rename.
+    
+6 ) Write short-cut keys to do the following operations:
+    a. Copy
+    b. Cut
+    c. Paste
+    d. print 
+Ans:Cut (Ctrl + X)
+    Copy (Ctrl + C)
+    Paste (Ctrl + V)
+    Print (Ctrl + P)
+    
+7 )Write a step by step process to delete a file on your system.
+Ans:To do so, right-click Start and choose Open Windows Explorer and then browse to locate the file you want to delete.
+    In Windows Explorer, right-click the file or folder that you want to delete and then choose Delete.
+    The Delete File dialog box appears. Click Yes to delete the file.
+    
+get_ipython().set_next_input('8 )What are some tasks performed by the file management system on a computer system');get_ipython().run_line_magic('pinfo', 'system')
+Ans:It helps to create new files in computer system and placing them at the specific locations.
+    It helps in easily and quickly locating these files in computer system.
+    It makes the process of sharing of the files among different users very easy and user friendly.
+
+
+ 
+ 
+
